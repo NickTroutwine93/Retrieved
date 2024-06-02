0584f23bd6e4b0d99d203c6ebc5501d06a08325d
Nick Troutwine <nicktroutwine93@gmail.com>
2024-06-02 15:12:31 -0500

    Time 5h Built first 2 tabs and search instance

--- src/App.js
+++ src/App.js
@@ -7,9 +7,14 @@ import Avatar from '@mui/material/Avatar';
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
-//import logo from "./assets/mapApprox.jpg";
+import backArrow from "./assets/back-arrow.png";
 import "./App.css";
 import LoadAccount from "./client/components/loadAccount.js";
+import CreateUserPetTile from "./client/components/getUserPets.js";
+import CreateYourSearchesTile from "./client/components/yourSearches.js";
+import OpenSearchInstnace from "./client/components/openSearchInstance.js";
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -40,39 +45,97 @@ const analytics = getAnalytics(app1);
 const db = getFirestore(app1); 
 let initialUserCall = false;
 function App() {
-	//data that will be injected as table from response
-	//#number of records will get updated with response as well
-	const [results, setResults] = React.useState("NT");
 	const [firstName, setFirstName] = React.useState("Unknown");
-	const [initials, setInitials] = React.useState("US");
-	//Variables that will be paramaters for get request. Value is set by input fields;
+	const [initials, setInitials] = React.useState("US"); 
 	const [profile, setProfile] = React.useState(null);
+	const [yourPets, setYourPets] = React.useState(null);
 	const [value, setValue] = React.useState('1');
+	const [userID, setuserID] = React.useState(null);
+	const [radius, setRadius] = React.useState(null);
+	const [search, setSearch] = React.useState(null);
+	const [homeView, setHomeView] = React.useState("block");
+	const [searchView, setsearchView] = React.useState("none");
+	const [searches, setSearches] = React.useState(null);
+	const [petTile, setPetTile] = React.useState(null);
+
+	
 
 	const handleChange = (event, newValue) => {
-	  setValue(newValue);
+		setValue(newValue);
+	};
+	const openSearch = (event) => {
+		let searchToOpen = event.currentTarget.getAttribute("data-searchid");
+		setHomeView("none");
+		setsearchView("block"); 
+		GetYourSearchesData(searchToOpen, false)
+	};
+	const closeSearch = (event) => {
+		console.log(event); 
+		setsearchView("none");
+		setHomeView("block");  
 	};
 	if(!initialUserCall){
-		GetData()
+		GetUserData()
 	}
-	async function GetData(){
+	async function GetUserData(){
 		
 		let q = query(collection(db, "accounts"), where("Email", "==", "test@gmail.com"));
-		const querySnapshot = await getDocs(q);
-		//console.log(querySnapshot);
-		querySnapshot.forEach((doc) => {
-		  // doc.data() is never undefined for query doc snapshots
+		const querySnapshot = await getDocs(q); 
+		querySnapshot.forEach((doc) => { 
 		  let userData = doc.data();
-		  console.log("Document data:",userData);
+		  setuserID(doc.id);
 		  if(userData){
 			initialUserCall = true
-		  }
-		  //console.log("dataCall",dataCall);
-		  //console.log(dataCall.totalHarvest,harvestInt); 
-			setProfile(LoadAccount(userData));
-			//setProfile(profileData);
+		  } 
+			setProfile(LoadAccount(userData)); 
 			setFirstName(userData.FirstName);
-			setInitials("NT");
+			setRadius(userData.radius);
+			setInitials("NT")
+			GetUserPetData(doc.id, 0)
+			GetYourSearchesData(doc.id, true)
+		});
+	};
+	async function GetUserPetData(lookupID, yourPet){
+		let q;
+		if(yourPet == 0){
+			q = query(collection(db, "pets"), where("OwnerID", "==", lookupID))
+		}else{
+			q = query(collection(db, "pets"), where("searchID", "==", lookupID))
+		};
+		const querySnapshot = await getDocs(q); 
+		querySnapshot.forEach((doc) => { 
+		  let userPetData = doc.data();
+		  let petID = doc.id;
+		  userPetData.PetID = petID;
+		  if(yourPet == 0){
+			setYourPets(CreateUserPetTile(userPetData)); 
+		  }
+		  if(yourPet == 1){
+			setSearches(CreateYourSearchesTile(userPetData,lookupID,openSearch)); 
+		  };
+		  if(yourPet == 2){
+			setPetTile(CreateYourSearchesTile(userPetData,lookupID, false)); 
+		  };
+		});
+	};
+	async function GetYourSearchesData(lookupID, yourSearch){
+		let q;
+		if(yourSearch){
+			q = query(collection(db, "searches"), where("searchersID", "array-contains", lookupID));
+		}else{ 
+			q = query(collection(db, "searches"), where('__name__', '==', lookupID))
+		};
+		const querySnapshot = await getDocs(q); 
+		querySnapshot.forEach((doc) => { 
+		  let yourSearchesData = doc.data();
+		  let searchID = doc.id;
+		  let petID = yourSearchesData.petID;
+		  if(yourSearch){
+			GetUserPetData(doc.id, 1)
+		  }else{
+			GetUserPetData(doc.id, 2);
+			setSearch(OpenSearchInstnace(yourSearchesData));
+		  };
 		});
 	};
 
@@ -84,24 +147,39 @@ function App() {
 				
 			</header>
 			<div className="app-body">
-				<Box sx={{ width: '100%', typography: 'body1' }}>
+				<Box style={{display: homeView}} id="Home">
 					<TabContext value={value}>
 						<Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
 						<TabList onChange={handleChange} aria-label="lab API tabs example">
-							<Tab label="Item One" value="1" />
-							<Tab label="Item Two" value="2" />
-							<Tab label="Item Three" value="3" />
+							<Tab label="Home" value="1" />
+							<Tab label="Your Searches" value="2" />
+							<Tab label="Searches in Area" value="3" />
 						</TabList>
 						</Box>
 						<TabPanel value="1">
 							Hello, {firstName}
 							
-							<div className="profile">{profile}</div>
-
+							<div className="profile">
+								{profile}
+								<div className="editBtnContainer">
+									<button className="editUser editBtn" data-userid={userID}>Edit</button>
+								</div>
+							</div>
+							<div className="petCards">{yourPets}</div>
+							<div className="addRemovePetContainer">
+								<button className="addRemovePet" data-userid={userID}>Add/Remove Pets</button>
+							</div> 
 						</TabPanel>
-						<TabPanel value="2">Item Two</TabPanel>
+						<TabPanel value="2">{searches}</TabPanel>
 						<TabPanel value="3">Item Three</TabPanel>
 					</TabContext>
+				</Box>
+				<Box  style={{display: searchView}}  id="Search">
+					<div className="closeSearch" onClick={closeSearch} style={{width: "100px",height: "50px"}}><img src={backArrow} id="BackArrow"/>  Back</div>
+					<div className="Search">
+						{petTile}
+						{search}
+					</div>
 				</Box>
 			</div>
 		</div>
--- src/client/components/yourSearches.js
+++ src/client/components/yourSearches.js
@@ -3,15 +3,19 @@ import Grid from '@mui/material/Grid';
 import logo from "../../assets/rigby.jpg";
 import Box from '@mui/material/Box';
 
-export default function CreateUserPetTile(petInfo) { 
+export default function CreateYourSearchesTile(petInfo,searchID,handleChange) { 
     let colorArray = petInfo.Color;
     let colorString ="Not Provided";
     if(colorArray){
         colorString= colorArray.join(", ")
     }
+    let tileClasses = "searchTile petTile"
+    if(!handleChange){
+        tileClasses = "petTile"
+    }
     return(
         <div style={{ height: 'auto', width: '100%' }}>
-            <Box className="petTile">
+            <Box className={tileClasses} onClick={handleChange ? handleChange : undefined } data-searchid={searchID}>
                 <Grid container spacing={2}>
                     <Grid item xs={6} md={8}>
                         <h2 className="petName">{petInfo.Name}</h2>
@@ -20,15 +24,9 @@ export default function CreateUserPetTile(petInfo) {
                         <div className="petRow breed">Size: {petInfo.Size}</div>
                     </Grid>
                     <Grid item xs={6} md={4}>
-                        <img src={logo} width="100%"/>
-                        <div className="editBtnContainer">
-                            <button className="editPet editBtn" data-petid={petInfo.PetID}>Edit</button>
-                        </div>
+                        <img src={logo} width="100%"/> 
                     </Grid>
-                </Grid>
-                <div className="createSearchContainer">
-                    <button className="createSearch" data-petid={petInfo.PetID} data-ownerid={petInfo.OwnerID}>Create Search</button>
-                </div>
+                </Grid> 
             </Box>
         </div>
     );
